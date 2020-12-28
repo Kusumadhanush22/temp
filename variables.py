62ed38aea75680dbae2d033d9be1a05d4faf2cce
Dhanush Kusuma <43772706+Kusumadhanush22@users.noreply.github.com>
2020-12-28 19:30:35 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+#variable declaration and rules.
+# a=5 gives you errorfirst letter should not be space
+a=5
+#varible name can have any symbol in keyboard excapt special symbols
+#variable name should not start with numbers
+a_=6
+_a_=8
+myVarible=8
+print(a)
+print(a_)
+print(_a_)
+print(myVarible)
